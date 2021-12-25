6eda93c72b1ac5f26bf5545d892f82249c0a56ab
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2021-12-25 23:02:03 +0200

    Fix several bugs

--- inputs.py
+++ inputs.py
@@ -1,59 +1,48 @@
 class Graph(object):
-    def __init__(self):
-        self.adjList = {}
+    def __init__(self, nodes, branches):
+        self.adjList = [[] for i in range(nodes)]
         self.branchName = {}
         self.invBranchName = {}
-        self.nodes = 0
-        self.branches = 0
+        self.nodes = nodes
+        self.branches = branches
 
     def addEdge(self, edge):
         fromNode, toNode = (edge)
-        if fromNode not in self.adjList:
-            self.adjList[fromNode] = []
         self.adjList[fromNode].append(toNode)
 
-    def readGraph(self, branchName, invBranchName, nodes, branches):
-        n, m = map(int, input().split())
-        nodes += n
-        branches += m
-        for branch in range(branches):
-            edge = tuple(map(int, input().split()))
-            self.addEdge(edge)
-            branchName[edge] = branch
-            invBranchName[branch] = edge
+    def readGraph(self, branchName, invBranchName):
+        for branch in range(self.branches):
+            (fromNode, toNode) = tuple(map(int, input().split()))
+            self.addEdge((fromNode - 1, toNode - 1))
+            branchName[(fromNode - 1, toNode - 1)] = self.branchName[(fromNode - 1, toNode - 1)] = branch
+            invBranchName[branch] = self.invBranchName[branch] = (fromNode - 1, toNode - 1)
 
     def dfs(self, node, visited, treeBranches, branchName, sameComponent, linked = True):
         if visited[node] == 1:
             return
         visited[node] = 1
-        sameComponent.add(node)
-
+        sameComponent[node] = True
         for child in self.adjList[node]:
             if visited[child] != 2:
                 if visited[child] != 1:
-                    treeBranches.append(branchName[{node, child}])
-                    self.dfs(child, visited, treeBranches, branchName, sameComponent, linked)
-            elif(linked and node not in sameComponent):
-                treeBranches.append(branchName[{node, child}])
+                    treeBranches.append(branchName[(node, child)])
+                self.dfs(child, visited, treeBranches, branchName, sameComponent, linked)
+            elif(linked and sameComponent[node] == False):
+                treeBranches.append(branchName[(node, child)])
                 linked = False
         visited[node] = 2
 
-    def findTree(self, graph, branchName, nodes, branches):
+    def findTree(self, branchName, nodes):
         visited = [0] * nodes
         treeBranches = []
-        sameComponent = set()
         for node in range(nodes):
             if visited[node] == 0:
-                sameComponent = set()
-                self.dfs(node, visited, treeBranches, branchName, sameComponent)
+                self.dfs(node, visited, treeBranches, branchName, [0] * nodes)
         return treeBranches
 
-
 def readCircuitComponents(branches):
-    ret = {}
+    ret = []
     for i in range(3):
-        ret[i] = []
-        for branch in range(branches):
-            now = eval(input())
-            ret[i].append(now)
+        ret.append(list(map(float, input().split())))
+    print(ret)
     return ret
