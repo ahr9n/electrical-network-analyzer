cc3abd6ec1cadae7ff525bd2b4b5679272e443c0
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2021-12-28 16:56:02 +0200

    Fix bugs in branches_order

--- outputs.py
+++ outputs.py
@@ -1,44 +1,53 @@
+from equations import *
 import numpy as np
 
 
-class Instructions(object):
+class Instructions:
     @staticmethod
-    def displayForMatrixA():
+    def display_for_matrix_a():
         print("         Welcome to the Electrical Network Analyzer!\n\n"
               "Input WiKi for matrix A:\n"
               "     The first line contains two integers; N, M:\n"
-              "     - N is the number of nodes, and M is the number of branches in the directed graph of the network, respectively.\n"
-              "     - The next M lines contain the edges of the graph, each in the form of two integers: U, V (1 <= U, V <= N);\n"
+              "     - N is the number of nodes, and M is the number of branches "
+              "in the directed graph of the network, respectively.\n"
+              "     - The next M lines contain the edges of the graph, "
+              "each in the form of two integers: U, V (1 <= U, V <= N);\n"
               "     (In other words: U and V are the end-points of the nodes that are connected by the branch).\n"
-              "Important Note:\n"
-              "     - The input order of the branches ARE SORTED from \'a\' (till the end using ASCII code).\n\n"
+              "\nImportant Note:\n"
+              "     - The input order of the branches ARE SORTED from \'1\' till the end.\n\n"
               "The input:\n")
 
     @staticmethod
-    def displayForMatrixB():
+    def display_for_matrix_b(tree_branches, reversed_branch_name):
+        branches_order = get_branches_order(tree_branches, reversed_branch_name)
+        branches_order = [branches_order[i] + 1 for i in range(len(branches_order))]
         print("\nInput WiKi for matrix B:\n"
-              "     The next three lines represent the voltage sources, the current sources and the resistances on the branches:\n"
-              "     Each line contains four values; b d a c (all respectively).\n")
+              "     The next three lines represent the voltage sources, "
+              "the current sources and the resistances on the branches:\n"
+              f"     Each line contains {len(branches_order)} values; {branches_order} (all respectively).\n")
+        print("The branches are in the following order:\n",
+              "     Voltage sources: ", branches_order, '\n',
+              "     Current sources: ", branches_order, '\n',
+              "     Resistors:       ", branches_order, '\n', sep='')
 
 
-class Formats(object):
+class Formats:
     @staticmethod
-    def formatMatrix(matrix, matrixType):
-        x = np.random.random(10)
+    def format_matrix(matrix, matrix_type):
         with np.printoptions(precision=5, suppress=True):
-            print("Matrix:", matrixType, '\n', np.array(matrix), '\n')
+            print("Matrix:", matrix_type, '\n', np.array(matrix), '\n')
 
     @staticmethod
-    def formatResult(matrixJBranch, matrixVBranch, branchesOrder):
+    def format_result(matrix_j_branch, matrix_v_branch, branches_order):
         print("The result:\n",
               "                    Voltage (V)     Current (A)\n",
               "     ---------      -----------     -----------")
-        for i in range(len(branchesOrder)):
-            print("      Branch ", branchesOrder[i] + 1, ":      ", end='', sep='')
-            if matrixVBranch[0][i] > 0:
+        for i in range(len(branches_order)):
+            print("      Branch ", branches_order[i] + 1, ":      ", end='', sep='')
+            if matrix_v_branch[0][i] > 0:
                 print(" ", end='')
-            print(round(matrixVBranch[0][i], 7), "      ", end='', sep='')
-            if matrixJBranch[0][i] > 0:
+            print(round(matrix_v_branch[0][i], 7), "      ", end='', sep='')
+            if matrix_j_branch[0][i] > 0:
                 print(" ", end='')
-            print(round(matrixJBranch[0][i], 7))
+            print(round(matrix_j_branch[0][i], 7))
         print("      ---------      -----------     -----------")
